fb16b601075175c0859ae80d1cf80090d35ed449
Bohdan Suprun <bogdansuprun96@gmail.com>
2017-09-28 13:03:20 +0300

    Install Bootstrap 3 Module

--- rss/apps/rss_news/views.py
+++ rss/apps/rss_news/views.py
@@ -1,8 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import random
+
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hi")
+    number = random.randrange(0, 100)
+    # return HttpResponse("Hi")
+    context = {
+        'value': 'Hello Python',
+        'title': 'Hello Django!',
+        'number': str(number),
+    }
+    return render(request, "index.html", context)
